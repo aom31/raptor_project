071e46266e4c72a04b7546985afed3da6ed44bc0
thamakorn <97218117+aom31@users.noreply.github.com>
2022-04-09 19:24:21 +0700

    Update serials_rover.py

--- gnss_positioning/scripts/serials_rover.py
+++ gnss_positioning/scripts/serials_rover.py
@@ -4,9 +4,10 @@ import time
 import pynmea2
 import matplotlib.pyplot as plt
 import math
-# ser = serial.Serial('COM6', baudrate=115200, timeout=1)
-# ser_bytes = ser.readline()
-port = "COM6"  
+
+
+
+port = "COM6"     #port usb that connect rover in your computer
 ser = serial.Serial(port, baudrate = 115200)
 while True:
     data = ser.readline()
@@ -14,12 +15,13 @@ while True:
     gngga_data = data.split(b",")
     # gngga_data[0] = b'$GNGGA'
     if gngga_data[0] == b"$GNGGA":
-        #print(data)
         newmsg=pynmea2.parse(data.decode("utf-8"))
         lat=newmsg.latitude
         lng=newmsg.longitude
-        gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
-        print(gps)
+        gps = [lat,lng] 
+        #gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+        print(gps)   #show data
+        time.sleep(dt)  # sleep(second) 5Hz = 1/5=0.2sec
         
         
     
